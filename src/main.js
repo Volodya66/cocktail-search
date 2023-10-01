537e48c0bd4f3d9d3f172be2729f68ffd5517de1
Imperface <kovzhenko.ua@gmail.com>
2023-10-01 07:23:33 +0300

    change file name

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './js/mobile-menu';
 import './js/theme-changer';
 import './js/render/render-search-letters';
-import './js/search-query';
+import './js/cocktails';
 import './js/user-screen-width-follower';
 import './js/fetch/get-random-cockt';
 import './js/render/render-coctails-cards';
