b50142b745fe3e009759edd761d846ad9f5e7251
Imperface <kovzhenko.ua@gmail.com>
2023-10-03 17:44:55 +0300

    fix cocktail modal

--- src/js/modal/modal-ingredient.js
+++ src/js/modal/modal-ingredient.js
@@ -22,7 +22,7 @@ async function selectedIngredient(evt) {
   let idIngredient = evt.target.getAttribute('id');
   // console.log(idIngredient);
   let dataSearchParam = await ingredientSearchRequest(idIngredient);
-  let markup = markupModalIngredients(dataSearchParam);
+  // let markup = markupModalIngredients(dataSearchParam);
 
   // ? додавання в до діва розмітки
   divModalIngredient.insertAdjacentHTML('afterbegin', markup);
@@ -30,42 +30,11 @@ async function selectedIngredient(evt) {
   const closeModalButton = document.querySelector('.js-close-btn');
   const closeModalBtnTwo = document.querySelector('.js-close-btn-two');
   // console.log(closeModalButton)
-  closeModalBtnTwo.addEventListener('click', closeModalIngredient);
-  closeModalButton.addEventListener('click', closeModalIngredient);
+  // closeModalBtnTwo.addEventListener('click', closeModalIngredient);
+  // closeModalButton.addEventListener('click', closeModalIngredient);
   function closeModalIngredient() {
     closeModal();
   }
-}
-
-function markupModalIngredients(objParam) {
-  return objParam
-    .map(({ type, title, description, abv, country, flavour }) => {
-      abv = abv || 'Not defined, so you will have to taste!';
-      country = country || 'There is no country of origin';
-      flavour = flavour || 'Not specified';
-      return `
-         <button class="close-modal-cocktails js-close-btn" data-modal-close-ingr>
-      <svg class="svg-close-modal-cocktails" width="22" height="22">
-        <use href="/img/sprite.svg#icon-close"></use>
-      </svg>
-    </button>
-    <h2 class="modal-ingredient-title">${title}</h2>
-    <p class="modal-ingredient-type">${type}</p>
-    <span class="lin"></span>
-    <p class="modal-ingredient-text"><span class="modal-ingredient-span-black">${title}</span>: ${description}</p>
-    <ul class="modal-ingredient-list-properties">
-      <li class="modal-ingredient-list-item">Type:${type}</li>
-      <li class="modal-ingredient-list-item">Country of origin:${country}</li>
-      <li class="modal-ingredient-list-item">Alcohol by volume:${abv}</li>
-      <li class="modal-ingredient-list-item">Flavour:${flavour}</li>
-    </ul>
-    <ul class="modal-ingredient-list-btn">
-      <button name="ingredient-add" class="modal-button modal-ingredient-add">add to favorite</button>
-      <button name="ingredient-back" class="modal-button js-close-btn-two modal-ingredient-back" data-modal-close>back</button>
-    </ul>
-        `;
-    })
-    .join('');
 }
 
 async function ingredientSearchRequest(searchParamId) {
