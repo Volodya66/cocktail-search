315905f1cfa2c34f0903ad9b84287007fafd4df8
Imperface <kovzhenko.ua@gmail.com>
2023-10-03 08:00:07 +0300

    fix ingred modal

--- src/js/modal/modal-ingredient.js
+++ src/js/modal/modal-ingredient.js
@@ -1,57 +1,49 @@
 // реалізувати реалізувати відкриття та закриття модалки.  функцію відмалювання розмітки модального вікна інградіента, з файлів element-add-to-local-storage.js та element-remove-from-local-storage імпортувати функції для додавання та видалення елементів local storage для додавання та видалення з улюбленого
 
-
 import axios from 'axios';
 
-
-const BASE_URL_INGREDIENT = "https://drinkify.b.goit.study/api/v1/ingredients/";
+const BASE_URL_INGREDIENT = 'https://drinkify.b.goit.study/api/v1/ingredients/';
 const divModalIngredient = document.querySelector('.modal-ingredient-details');
 const modal = document.querySelector('.modal-ingredients');
-
-
+const modalCocktail = document.querySelector('.wrapper-modal-cocktails');
 // const listCocktail = document.querySelector('.list-cocktail-ingredients');
 // listCocktail.addEventListener('click', selectedIngredient)
 
+async function selectedIngredient(evt) {
+  if (evt.target.nodeName !== 'LI') {
+    return;
+  }
+  console.log(modal);
+  openModal();
+  divModalIngredient.innerHTML = '';
+  // openModal();
 
+  // const btnModalIngredientBack = document.querySelector('.modal-ingredient-back')
+  let idIngredient = evt.target.getAttribute('id');
+  // console.log(idIngredient);
+  let dataSearchParam = await ingredientSearchRequest(idIngredient);
+  let markup = markupModalIngredients(dataSearchParam);
 
+  // ? додавання в до діва розмітки
+  divModalIngredient.insertAdjacentHTML('afterbegin', markup);
 
-async function  selectedIngredient(evt) {
+  const closeModalButton = document.querySelector('.js-close-btn');
+  const closeModalBtnTwo = document.querySelector('.js-close-btn-two');
+  // console.log(closeModalButton)
+  closeModalBtnTwo.addEventListener('click', closeModalIngredient);
+  closeModalButton.addEventListener('click', closeModalIngredient);
+  function closeModalIngredient() {
+    closeModal();
+  }
+}
 
-    if (evt.target.nodeName !== "LI") {
-        return
-    }
-    console.log(modal)
-    openModal();
-    divModalIngredient.innerHTML = '';
-    // openModal();
-   
-    // const btnModalIngredientBack = document.querySelector('.modal-ingredient-back')
-    let idIngredient = evt.target.getAttribute('id');
-    // console.log(idIngredient);
-    let dataSearchParam = await ingredientSearchRequest(idIngredient);
-    let markup = markupModalIngredients(dataSearchParam);
-    
-    // ? додавання в до діва розмітки 
-    divModalIngredient.insertAdjacentHTML("afterbegin", markup);
-
-    const closeModalButton = document.querySelector('.js-close-btn');
-    const closeModalBtnTwo = document.querySelector('.js-close-btn-two')
-    // console.log(closeModalButton)
-    closeModalBtnTwo.addEventListener('click', closeModalIngredient)
-    closeModalButton.addEventListener('click', closeModalIngredient)
-    function closeModalIngredient() {
-      closeModal();
-      
-    } 
-};
-
-function markupModalIngredients(objParam){
-    
-    return objParam.map(({ type, title, description, abv, country, flavour }) => {
-        abv = abv || "Not defined, so you will have to taste!";
-        country = country || "There is no country of origin";
-        flavour = flavour || "Not specified";
-        return `
+function markupModalIngredients(objParam) {
+  return objParam
+    .map(({ type, title, description, abv, country, flavour }) => {
+      abv = abv || 'Not defined, so you will have to taste!';
+      country = country || 'There is no country of origin';
+      flavour = flavour || 'Not specified';
+      return `
          <button class="close-modal-cocktails js-close-btn" data-modal-close-ingr>
       <svg class="svg-close-modal-cocktails" width="22" height="22">
         <use href="/img/sprite.svg#icon-close"></use>
@@ -71,30 +63,35 @@ function markupModalIngredients(objParam){
       <button name="ingredient-add" class="modal-button modal-ingredient-add">add to favorite</button>
       <button name="ingredient-back" class="modal-button js-close-btn-two modal-ingredient-back" data-modal-close>back</button>
     </ul>
-        `
-    }).join('')
-
-};
+        `;
+    })
+    .join('');
+}
 
 async function ingredientSearchRequest(searchParamId) {
-    try {
-        let responseForIngr = await axios.get(`${BASE_URL_INGREDIENT}${searchParamId}`)
-        // console.log(responseForIngr.data)
-        return responseForIngr.data
-    }
-    catch {
-        console.log(error.message)
-    }
-
+  try {
+    let responseForIngr = await axios.get(
+      `${BASE_URL_INGREDIENT}${searchParamId}`
+    );
+    // console.log(responseForIngr.data)
+    return responseForIngr.data;
+  } catch {
+    console.log(error.message);
+  }
 }
 
 function openModal() {
-    modal.classList.remove('is-hidden');
+  const cocktailsModalCloseBtn = document.querySelector('.cocktails-close');
+  cocktailsModalCloseBtn.setAttribute('disabled', 'true');
+  modalCocktail.classList.add('semi-hidden');
+  modal.classList.remove('is-hidden');
 }
-
 
 function closeModal() {
-    modal.classList.add('is-hidden');
+  const cocktailsModalCloseBtn = document.querySelector('.cocktails-close');
+  cocktailsModalCloseBtn.removeAttribute('disabled');
+  modal.classList.add('is-hidden');
+  modalCocktail.classList.remove('semi-hidden');
 }
 
-export {selectedIngredient}
+export { selectedIngredient };
