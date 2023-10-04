8ee7aa64c612b62c183d934a62354892aff32411
DianaTel <dianatelcharova@gmail.com>
2023-10-04 19:43:49 +0300

    chButton

--- src/js/theme-changer.js
+++ src/js/theme-changer.js
@@ -1,44 +1,32 @@
 // реалізувати логіку перемикача теми сайту та зробити іменований експорт змінної зі значенням true, якщо темна тема увімкнена, false, якщо темна тема вимкнена, також реалізувати логіку додавання цього значення у властивість в localStorage, та зчитування цього, якщо користувач вже відвідував сайт і вибирав тему
 
-const btnChangeThemeRef = document.querySelectorAll('#js-btn-change-theme');
-function onBtnChangerClick(e) {
-  const elementDarkThemeChange = [
-    ...document.querySelectorAll('.change-theme'),
-  ];
+const btnChangeThemeRef = document.querySelector('#js-btn-change-theme');
+const elementDarkThemeChange = [...document.querySelectorAll('.change-theme')];
+
+function saveDarkModeState(isDarkModeEnabled) {
+  localStorage.setItem('darkMode', isDarkModeEnabled);
+}
+
+function loadDarkModeState() {
+  return localStorage.getItem('darkMode') === 'true';
+}
+
+const isDarkModeEnabled = loadDarkModeState();
+elementDarkThemeChange.forEach(element => {
+  element.classList.toggle('dark-mode', isDarkModeEnabled);
+});
+
+if (isDarkModeEnabled) {
+  btnChangeThemeRef.classList.add('clicked');
+}
+
+btnChangeThemeRef.addEventListener('click', () => {
   elementDarkThemeChange.forEach(element => {
     element.classList.toggle('dark-mode');
   });
-}
-btnChangeThemeRef.forEach(element => {
-  element.addEventListener('click', onBtnChangerClick);
+
+  const isDarkModeToggled = !isDarkModeEnabled;
+  saveDarkModeState(isDarkModeToggled);
+
+  btnChangeThemeRef.classList.toggle('clicked');
 });
-
-// const btnChangeThemeRef = document.querySelector('#js-btn-change-theme');
-
-// function onBtnChangerClick(e) {
-//   const elementDarkThemeChange = [
-//     ...document.querySelectorAll('.change-theme'),
-//   ];
-
-//   const isDarkModeEnabled = localStorage.getItem('darkMode') === 'true';
-
-//   elementDarkThemeChange.forEach(element => {
-//     element.classList.toggle('dark-mode', isDarkModeEnabled);
-//   });
-
-//   localStorage.setItem('darkMode', !isDarkModeEnabled);
-// }
-
-// btnChangeThemeRef.addEventListener('click', onBtnChangerClick);
-
-// window.addEventListener('load', () => {
-//   const isDarkModeEnabled = localStorage.getItem('darkMode') === 'true';
-
-//   const elementDarkThemeChange = [
-//     ...document.querySelectorAll('.change-theme'),
-//   ];
-
-//   elementDarkThemeChange.forEach(element => {
-//     element.classList.toggle('dark-mode', isDarkModeEnabled);
-//   });
-// });
